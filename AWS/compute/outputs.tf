717553178b9334faba762539d08c2cdff3904d5b
mvenkata11 <mvenkata11@gmail.com>
2019-03-19 19:35:03 -0700

    added outputs for compute and root

---
+++ AWS/compute/outputs.tf
@@ -0,0 +1,11 @@
+output "server_id" {
+
+value ="${join(",",aws_instance.tf_server.*.id)}"
+
+}
+
+output "server_ip" {
+
+value = "${join(",",aws_instance.tf_server.*.public_ip)}"
+
+}
--- AWS/outputs.tf
+++ AWS/outputs.tf
@@ -1,3 +1,29 @@
 output "bucket_name"{
 value ="${module.storage.bucketname}
 }
+
+output "public subnets"{
+
+value = "${join(",", module.network.public_subnets)}"
+}
+
+output "subnet ips"{
+
+value = "${join(",", module.network.subnet_ips)}"
+
+}
+
+output "public ssecurity group"{
+value = "${module.network.public_sg"}"
+}
+
+output "public instance ids" {
+value = "${module.compute.server_id}"
+
+}
+
+output "public instance ips" {
+value = "${module.compute.server_ip}"
+
+}
+
