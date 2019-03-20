a04acc23b0c3be1a4a82dfdd3efd1e83d2c78b95
mvenkata11 <mvenkata11@gmail.com>
2019-03-19 19:20:16 -0700

    added inatnce and keypair in compute and imported this module in root

---
+++ AWS/compute/variables.tf
@@ -0,0 +1,35 @@
+variable "key_name" {
+
+
+
+}
+
+variable "public_key_path"{
+
+}
+
+variable "subnet_ips"{
+type ="list"
+}
+variable "instance_count" {
+
+}
+
+variable "instance_type" {
+
+}
+
+variable "security_group" {
+
+}
+
+variable "subnets" {
+type = "list"
+}
+
+variable "key_name"{ }
+variable "public_key_path {}
+variable "server_instance_type {}
+variable "instance_count" {
+default =1
+}
--- AWS/main.tf
+++ AWS/main.tf
@@ -18,6 +18,18 @@ vpc_cidr = "${var.vpc_cidr}"
 public_cidrs ="${var.vpc_cidrs}"
 accessip = "${var.accessip}
 
+}
 
+
+module "compute" {
+
+source = "./compute"
+instance_count = "${var.instance_count}"
+key_name ="${var.key_name}"
+public_key_path = "${var.public_key_path}"
+instance_type = "${var.server_instance_type}"
+subnets = "${module.network.public_subnets}"
+security_group = "${module.network.public_sg}"
+subnet_ips = "${module.network.subnet_ips}"
 
 }
